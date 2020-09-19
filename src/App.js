b7a7ffba117b0b29cc7fd52386c9752cbebad4fe
Justin Carter <justin@vidpresso.com>
2020-09-19 16:17:41 -0400

    tweaked

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { useState } from 'react';
 import './App.css';
 
+const employees = ["Eli", "Phoebe"];
+
 const itemCost = 1;
 const products = [
-  { name: "fudge brownie", cost: itemCost },
-  { name: "Double Chocolate Cookies (x2)", cost: itemCost },
-  { name: "Peanut Butter Brownie", cost: itemCost },
-  { name: "Pumpkin Spice Crispy Treat", cost: itemCost },
-  { name: "Pumpkin Spice Roll", cost: itemCost },
+  { name: "fudge brownie", img: "fudgebrownie.jpg", cost: itemCost },
+  { name: "Double Chocolate Cookies (x2)", img: "doublechocolatecookies.jpg", cost: itemCost },
+  { name: "Peanut Butter Brownie", img: "peanutbutterbrownie.jpg", cost: itemCost },
+  { name: "Pumpkin Spice Crispy Treat", img: "pumpkinspicecrispytreat.jpg", cost: itemCost },
+  { name: "Pumpkin Spice Roll", img: "pumpkinspiceroll.jpg", cost: itemCost },
+  { name: "Sugar Cookie", img: "sugarcookie.jpg", cost: itemCost },
 ]
 const cashAmounts = [
   1, 5, 10, 20
@@ -19,19 +22,41 @@ const minWidth = 60;
 
 function App() {
   const [purchaseAnalytics, setPurchaseAnalytics] = useState({});
-  const [currentCart, setCurrentCart] = useState(0);
+  const [currentCart, setCurrentCart] = useState(Array(products.length).fill(0));
   const [pendingAmountTendered, setPendingAmountTendered] = useState(0);
   const [changeGuess, setChangeGuess] = useState(0);
+  const [guessedCorrect, setGuessedCorrect] = useState(null);
   const [totalAmountRaised, setTotalAmountRaised] = useState(window.localStorage.getItem('totalAmountRaised') || 0);
 
   const [totalItemsSold, setTotalItemsSold] = useState(window.localStorage.getItem('totalItemsSold') || 0);
-  const productElements = products.map((product) => {
+  const [actualChange, setActualChange] = useState(0);
+  const [currentEmployee, setCurrentEmployee] = useState();
+
+  const setCurrentCartHandler = (value, index) => {
+    const newCurrentCart = [...currentCart];
+    newCurrentCart[index] = currentCart[index] + value;
+    setCurrentCart(newCurrentCart);
+  }
+
+  const currentCartTotal = currentCart.reduce((total, current) => {
+    return total + current;
+  }, 0)
+  const productElements = products.map((product, index) => {
     return (
-      <button style={{ height, minWidth }} key={product.name} onClick={() => {
-        setCurrentCart(currentCart + product.cost)
-      }}>
-        {product.name}
-      </button>
+      <div
+        className={`product ${currentCart[index] > 0 ? 'selected' : ''}`}
+        key={product.name}
+        onClick={() => {
+          setCurrentCartHandler(product.cost, index)
+        }}
+      >
+        <div className="productPicture" style={{background: `url(${product.img}) no-repeat 50% 50%/cover`}}>
+          {currentCart[index] > 0 && <div className="productCount">{currentCart[index]}</div>}
+        </div>
+        <div className="productText">
+          {product.name}
+        </div>
+      </div>
     );
   })
   const cashButtons = cashAmounts.map((cashAmount) => {
@@ -51,48 +76,79 @@ function App() {
   changeButtons.push(<button style={{ height, minWidth }} onClick={() => {
     setChangeGuess(0);
   }}>reset</button>)
-  const [actualChange, setActualChange] = useState(0);
-  const employees = ["Eli", "Phoebe"];
-  const [currentEmployee, setCurrentEmployee] = useState();
   const employeeResetElements = employees.map((employee) => {
-    return (<button style={{ height, minWidth }} onClick={() => {
-      setCurrentEmployee(employee);
-      setCurrentCart(0);
-      setPendingAmountTendered(0);
-      setActualChange(0);
-      setChangeGuess(0);
-      setGuessedCorrect(null);
-    }} key={employee}>{employee}</button>)
+    return (
+      <div
+        className={`employee ${currentEmployee === employee ? 'selected' : ''}`}
+        key={employee}
+        onClick={() => {
+          setCurrentEmployee(employee);
+          setCurrentCart(Array(products.length).fill(0));
+          setPendingAmountTendered(0);
+          setActualChange(0);
+          setChangeGuess(0);
+          setGuessedCorrect(null);
+        }}
+      >
+        <div className={`employeePicture ${employee.toLowerCase()}`} />
+        <div className="employeeName">{employee}</div>
+      </div>
+    )
   })
-  const [guessedCorrect, setGuessedCorrect] = useState(null);
   return (
     <div>
-      <div>Ready for the next employee?</div>
-      {employeeResetElements}
-      <h2> Current employee: {currentEmployee} </h2>
-      {productElements}
-      <div>Cart total: <b>${currentCart}.00</b></div>
-      <div>Enter cash total</div>
-      {cashButtons}
-      <div>Cash tendered: <b>${pendingAmountTendered}.00</b> </div>
-      <div>How much change do you think it'll be?</div>
-      {changeButtons}
-      <div>Change estimated total: <b>${changeGuess}.00</b></div>
-      <button style={{ height, minWidth }} onClick={() => {
-        const change = pendingAmountTendered - currentCart;
-        setActualChange(change);
-        if (change === changeGuess) {
-          setGuessedCorrect(true);
-        }
-        const newTotalAmount = currentCart + totalAmountRaised;
-        const newTotalItemsSold = (currentCart / itemCost) + totalItemsSold;
-        setTotalAmountRaised(newTotalAmount);
-        setTotalItemsSold(newTotalItemsSold);
-        window.localStorage.setItem('totalAmount', newTotalAmount);
-        window.localStorage.setItem('totalItemsSold', newTotalItemsSold);
-      }}>Check out</button>
-      <div>Their actual change is <b>${actualChange}.00</b></div>
-      <div>You estimated <b>${changeGuess}.00</b>. Your estimation was <b>{guessedCorrect === true ? "right!" : "incorrect."}</b></div>
+      <div className="employeeRow">{employeeResetElements}</div>
+      <div className="productRow">{productElements}</div>
+
+      <div className="moneyRow">
+        <div className="moneyItem">
+          <div className="moneyText">They owe you</div>
+          <div className="moneyNumber">${currentCartTotal}</div>
+        </div>
+        <div className="moneyItem">
+          <div className="moneyText">They gave you</div>
+          <div className="moneyNumber">${pendingAmountTendered}</div>
+          <div className="moneyButtonRow">{cashButtons}</div>
+        </div>
+        <div className="moneyItem">
+          <div className="moneyText">How much you think you should give back</div>
+          <div>
+            {guessedCorrect == null ? (
+              <div className="moneyNumber">${changeGuess}</div>
+            ) : (
+              guessedCorrect === true ? (
+                <div className="moneyNumber">${changeGuess} <span className="tada">🎉</span></div>
+              ) : (
+                <div className="moneyNumber">
+                  <span className="strike">${changeGuess}</span>{' '}
+                  ${actualChange}
+                </div>
+              )
+            )}
+          </div>
+          <div className="moneyButtonRow">{changeButtons}</div>
+        </div>
+        <div className="moneyItem shrink">
+          <div
+            className={`checkButton ${guessedCorrect != null ? 'checked' : ''}`}
+            onClick={() => {
+              const change = pendingAmountTendered - currentCartTotal;
+              setActualChange(change);
+              if (change === changeGuess) {
+                setGuessedCorrect(true);
+              } else {
+                setGuessedCorrect(false);
+              }
+              const newTotalAmount = currentCartTotal + totalAmountRaised;
+              const newTotalItemsSold = (currentCartTotal / itemCost) + parseInt(totalItemsSold, 10);
+              setTotalAmountRaised(newTotalAmount);
+              setTotalItemsSold(newTotalItemsSold);
+              window.localStorage.setItem('totalAmount', newTotalAmount);
+              window.localStorage.setItem('totalItemsSold', newTotalItemsSold);
+            }}
+          >Check</div>
+        </div>
+      </div>
 
 
       <h2>Bake sale totals:</h2>
